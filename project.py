e8ab16ecac73077df9193ee87d9a952b9246f383
Isaac Martinec <isaacmegaman007@gmail.com>
2024-04-24 17:31:15 -0400

    Welcome Page Made

--- project.py
+++ project.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+   return render_template('home.html')
 
 if __name__ == "__main__":
     app.run()
